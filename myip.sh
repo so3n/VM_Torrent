9e83c3b2fd5976ccda3f3b50dc7dd527110a0e9c
so3n <nicholas.neos@gmail.com>
2018-10-01 01:46:06 +1000

    updated myip script

--- myip.sh
+++ myip.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "===== Current User ====="
-wget http://ipinfo.io/ip -qO -
+#wget http://ipinfo.io/ip -qO -
 curl ipinfo.io
 echo "\n===== vpn user ====="
-sudo -u vpn -i -- wget http://ipinfo.io/ip -qO -
+#sudo -u vpn -i -- wget http://ipinfo.io/ip -qO -
 sudo -u vpn -i -- curl ipinfo.io
 
