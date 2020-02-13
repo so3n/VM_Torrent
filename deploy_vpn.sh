fccf771e8a69750b86ef5847f988ac2d7b847876
Nick Neos <nicholas.neos@gmail.com>
2020-02-13 23:04:52 +1100

    formatting changes to deploy_vpn.sh

--- deploy_vpn.sh
+++ deploy_vpn.sh
@@ -1,165 +1,29 @@
 #!/bin/bash
-###
 # Based on https://www.htpcguides.com/force-torrent-traffic-vpn-split-tunnel-debian-8-ubuntu-16-04/
-###
 
-###
-# Functions
-###
-prompt()
+
+setup_env()
 {
-    echo ""
-    read -rsp $'Press enter to continue...\n'
-}
-
-beautify(){
-    case $2 in
-        1)
-            echo -e "\n############### $1 ###############\n"
-            ;;
-        2)
-            echo -e "\n***$1***\n"
-            ;;
-        3)
-            echo -e "\n+++$1+++\n"
-            ;;
-        *)
-            echo -e "\n############### $1 ###############\n"
-            ;;
-    esac
-}
-
-setup_var()
-{
-    # variables
     SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     PIA_USER="piauser"
     PIA_PW="abc123"
     NET_IF=$(ip -o link show | sed -rn '/^[0-9]+: en/{s/.: ([^:]*):.*/\1/p}')
     LOCAL_IP=$(/sbin/ip -o -4 addr list $NET_IF | awk '{print $4}' | cut -d/ -f1)
+	
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    YELLOW="\033[1;33m"
+    NC="\033[0m" # No Color
 
     if [ $SUDO_USER ]; then
         REAL_USER=$SUDO_USER
     else
         REAL_USER=$(whoami)
     fi
-}
 
-install_packages()
-{
-    beautify "Install OpenVPN"
-    wget https://swupdate.openvpn.net/repos/repo-public.gpg -O - | apt-key add -
-    echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" | tee -a /etc/apt/sources.list.d/openvpn.list
-    apt update
-    apt install openvpn -y
-    echo -e "\nDone"
-
-    beautify "install other packages" 3
-    apt install unzip curl vim htop software-properties-common -y
-    echo -e "\nDone"
-}
-
-openvpn_setup()
-    {
-    beautify "Create systemd Service for OpenVPN"
-    cp src/openvpn@openvpn.service /etc/systemd/system/
-    echo "Enabling openvpn@openvpn.service....."
-    systemctl enable openvpn@openvpn.service
-    echo -e "\nDone"
-
-    beautify "Create PIA Configuration File for Split Tunneling"
-    beautify "Get the Required Certificates for PIA" 3
-    cd /tmp
-    wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
-    unzip openvpn.zip
-    cp crl.rsa.2048.pem ca.rsa.2048.crt /etc/openvpn/
     cd $SCRIPT_DIR
-    echo -e "\nDone"
 
-    beautify "Create Modified PIA Configuration File for Split Tunneling" 3
-    cp src/openvpn.conf /etc/openvpn/
-    echo -e "\nDone"
-
-    beautify "Make OpenVPN Auto Login on Service Start" 3
-    echo $PIA_USER | tee /etc/openvpn/login.txt
-    echo $PIA_PW | tee -a /etc/openvpn/login.txt
-    echo -e "\nDone"
-
-    beautify "Configure VPN DNS Servers to Stop DNS Leaks" 3
-    cp src/update-resolv-conf /etc/openvpn/
-    echo -e "\nDone"
-
-
-    beautify "Split Tunneling with iptables and Routing Tables"
-    beautify "Create vpn User" 3
-    adduser --disabled-login vpn
-    usermod -aG vpn $REAL_USER
-    usermod -aG $REAL_USER vpn
-    echo -e "\nDone"
-
-    beautify "Block vpn user access to internet" 3
-    iptables -F
-    iptables -A OUTPUT ! -o lo -m owner --uid-owner vpn -j DROP
-    apt install iptables-persistent -y
-    echo -e "\nDone"
-
-    beautify "iptables Script for vpn User" 3
-    cp src/iptables.sh /etc/openvpn/
-    chmod +x /etc/openvpn/iptables.sh
-    sed -i "s/192.168.1.100/$LOCAL_IP/" /etc/openvpn/iptables.sh
-    sed -i "s/eth0/$NET_IF/" /etc/openvpn/iptables.sh
-    beautify "showing first few lines of iptables.sh" 2
-    head -8 /etc/openvpn/iptables.sh
-    echo -e "\n*************************************************\n" 
-    echo -e "\nDone"   
-    
-    beautify "Routing Rules Script for the Marked Packets" 3
-    cp src/routing.sh /etc/openvpn/
-    chmod +x /etc/openvpn/routing.sh
-    echo -e "\nDone"
-
-    beautify "Configure Split Tunnel VPN Routing" 3
-    echo "200     vpn" | tee -a /etc/iproute2/rt_tables
-    beautify "showing last few lines of /etc/iproute2/rt_tables" 2
-    tail /etc/iproute2/rt_tables
-    echo -e "\n*************************************************\n"  
-    echo -e "\nDone"  
-
-    beautify "Change Reverse Path Filtering" 3
-    cp src/9999-vpn.conf /etc/sysctl.d/
-    sed -i "s/eth0/$NET_IF/" /etc/sysctl.d/9999-vpn.conf
-    beautify "showing first few lines of 9999-vpn.conf" 2
-    head /etc/sysctl.d/9999-vpn.conf
-    echo -e "\n*************************************************\n"    
-    sysctl --system
-    
-    beautify "Keep Alive Script"
-    cp src/vpn_keepalive.sh /etc/openvpn/
-    chmod +x /etc/openvpn/vpn_keepalive.sh
-    sed -i "s|/PATH/TO|$SCRIPT_DIR|" /etc/openvpn/vpn_keepalive.sh
-    beautify "showing last few lines of vpn_keepalive.sh" 2
-    tail /etc/openvpn/vpn_keepalive.sh
-    echo -e "\n*************************************************\n" 
-
-    echo -e "\nDone"
-
-
-}
-
-###
-# Main body of script starts here
-###
-if ! [ $(id -u) = 0 ]; then
-   echo "The script need to be run as root." >&2
-   exit 1
-fi
-
-set -e
-clear
-setup_var
-cd $SCRIPT_DIR
-
-echo -e "
+    echo -e "
 #################################################################
 Script Directory:  $SCRIPT_DIR
 Current User:      $REAL_USER
@@ -168,10 +32,123 @@ PIA Password:      $PIA_PW
 Network Interface: $NET_IF
 Local IP:          $LOCAL_IP
 #################################################################
-\n"
-prompt
+    ${NC}\n"
+}
 
+install_packages()
+{
+    # Install Packages
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Install Packages${NC}\n"
+    wget https://swupdate.openvpn.net/repos/repo-public.gpg -O - | apt-key add -
+    echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" | tee -a /etc/apt/sources.list.d/openvpn.list
+    apt update
+    apt install openvpn unzip curl vim htop software-properties-common -y
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+}
+
+openvpn_setup()
+{
+    # Create systemd Service for OpenVPN
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Enabling openvpn@openvpn.service${NC}\n"
+    cp src/openvpn@openvpn.service /etc/systemd/system/
+    systemctl enable openvpn@openvpn.service
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    # Create PIA Configuration File for Split Tunneling
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Get the Required Certificates for PIA${NC}\n"
+    cd /tmp
+    wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
+    unzip openvpn.zip
+    cp crl.rsa.2048.pem ca.rsa.2048.crt /etc/openvpn/
+    cd $SCRIPT_DIR
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Create Modified PIA Configuration File for Split Tunneling${NC}\n"
+    cp src/openvpn.conf /etc/openvpn/
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Make OpenVPN Auto Login on Service Start${NC}\n"
+    echo $PIA_USER | tee /etc/openvpn/login.txt
+    echo $PIA_PW | tee -a /etc/openvpn/login.txt
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Configure VPN DNS Servers to Stop DNS Leaks${NC}\n"
+    cp src/update-resolv-conf /etc/openvpn/
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    # Split Tunneling with iptables and Routing Tables
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Create vpn User${NC}\n"
+    adduser --disabled-login vpn
+    usermod -aG vpn $REAL_USER
+    usermod -aG $REAL_USER vpn
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Block vpn user access to internet${NC}\n"
+    iptables -F
+    iptables -A OUTPUT ! -o lo -m owner --uid-owner vpn -j DROP
+    apt install iptables-persistent -y
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') iptables Script for vpn User${NC}\n"
+    cp src/iptables.sh /etc/openvpn/
+    chmod +x /etc/openvpn/iptables.sh
+    sed -i "s/192.168.1.100/$LOCAL_IP/" /etc/openvpn/iptables.sh
+    sed -i "s/eth0/$NET_IF/" /etc/openvpn/iptables.sh
+    # echo -e "showing first few lines of iptables.sh" 2
+    # head -8 /etc/openvpn/iptables.sh
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"   
+    
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Routing Rules Script for the Marked Packets${NC}\n"
+    cp src/routing.sh /etc/openvpn/
+    chmod +x /etc/openvpn/routing.sh
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Configure Split Tunnel VPN Routing${NC}\n"
+    echo "200     vpn" | tee -a /etc/iproute2/rt_tables
+    # echo -e "showing last few lines of /etc/iproute2/rt_tables" 2
+    # tail /etc/iproute2/rt_tables
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"  
+
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Change Reverse Path Filtering${NC}\n"
+    cp src/9999-vpn.conf /etc/sysctl.d/
+    sed -i "s/eth0/$NET_IF/" /etc/sysctl.d/9999-vpn.conf
+    # echo -e "showing first few lines of 9999-vpn.conf" 2
+    # head /etc/sysctl.d/9999-vpn.conf   
+    sysctl --system
+    
+    # Keep Alive Script
+    echo -e "\n${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') Setup Keep Alive Script${NC}\n"
+    cp src/vpn_keepalive.sh /etc/openvpn/
+    chmod +x /etc/openvpn/vpn_keepalive.sh
+    sed -i "s|/PATH/TO|$SCRIPT_DIR|" /etc/openvpn/vpn_keepalive.sh
+    # echo -e "showing last few lines of vpn_keepalive.sh" 2
+    # tail /etc/openvpn/vpn_keepalive.sh
+    echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') Done${NC}\n"
+}
+
+prompt()
+{
+    echo ""
+    read -rsp $'Press enter to continue...\n'
+}
+
+error() {
+    echo -e "${RED}$(date '+%Y-%m-%d %H:%M:%S') Exiting due to Error${NC}\n"
+}
+
+
+### LET'S DO THIS!
+
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+set -e
+trap error ERR
+clear
+setup_env
+prompt
 install_packages
 openvpn_setup
-
-echo -e "\nCOMPLETE...reboot system to take effect\n"
+echo -e "\n${GREEN}$(date '+%Y-%m-%d %H:%M:%S') COMPLETE...reboot system to take effect${NC}\n"
